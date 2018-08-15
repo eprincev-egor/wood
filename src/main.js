757fe89981e7f354c8ecd9805df6f5f7f5429e3f
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2018-08-15 21:14:44 +0300

    in work

--- src/main.js
+++ src/main.js
@@ -1,78 +1,112 @@
 "use strict";
 
 const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
-
 canvas.style.position = "fixed";
 canvas.style.width = "100%";
 canvas.style.height = "100%";
-setTimeout(() => {
-    document.body.appendChild(canvas);
-});
+
+const gl = canvas.getContext("webgl");
 
 let width, height;
-let camera = {
-    x: 0,
-    y: 0,
-    step: 10
-};
 let state = {
     keyboard: {
         up: false,
         down: false,
         left: false,
         right: false
-    },
-    tree: [{
-        x: 0,
-        y: 0,
-        r: 20
-    }]
-};
-
-let draw = {
-    tree(tree) {
-        let {x, y, r} = tree;
-        let 
-            x0 = x + width / 2,
-            y0 = y + height / 2,
-            r0 = r,
-
-            x1 = x0 - 30,
-            y1 = y0 - 30,
-            r1 = r * 2;
-        
-        ctx.fillStyle = "orange";
-        ctx.beginPath();
-        ctx.arc(x0, y0, r0, 0, 2 * Math.PI);
-        ctx.fill();
-
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.arc(x1, y1, r1, 0, 2 * Math.PI);
-        ctx.fill();
     }
 };
 
-setInterval(() => {
+
+function createShader(type, source) {
+    let shader = gl.createShader(type);
+    gl.shaderSource(type, source);
+    gl.compileShader(shader);
+    return shader;
+}
+
+function createProgramm(vertexShader, fragmentShader) {
+    let programm = gl.createProgram();
+    
+    gl.attachShader(programm, vertexShader);
+    gl.attachShader(programm, fragmentShader);
+
+    gl.linkProgram(programm);
+
+    return programm;
+}
+
+
+setTimeout(() => {
+    document.body.appendChild(canvas);
     width = canvas.width = document.body.offsetWidth,
     height = canvas.height = document.body.offsetHeight;
-    
-    if ( state.keyboard.up ) {
-        camera.y -= camera.step;
-    }
-    if ( state.keyboard.down ) {
-        camera.y += camera.step;
-    }
-    if ( state.keyboard.left ) {
-        camera.x -= camera.step;
-    }
-    if ( state.keyboard.right ) {
-        camera.x += camera.step;
-    }
 
-    state.tree.forEach(draw.tree);
-}, 30);
+
+    let vertexShader = createShader(gl.VERTEX_SHADER, `
+        attribute vec4 a_position;
+
+        void main() {
+            gl_Position = a_position;
+        }
+    `);
+    let fragmentShader = createShader(gl.FRAGMENT_SHADER, `
+        precision mediump float;
+
+        void main() {
+            gl_FragColor = vec4(1, 0, 0.5, 1);
+        }
+    `);
+
+    let programm = createProgramm(vertexShader, fragmentShader);
+
+    let positionAttr = gl.getAttribLocation(programm, "a_posiiton");
+    let positionBuffer = gl.createBuffer();
+    let positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0
+    ];
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    gl.viewport(0, 0, width, height);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(programm);
+
+    gl.enableVertexAttribArray(positionAttr);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    gl.vertexAttribPointer(
+        positionAttr,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+
+    gl.drawArrays(
+        gl.TRIANGLES,
+        0,
+        3
+    );
+});
+
+
+
+
+// ===
+// setInterval(() => {
+//     width = canvas.width = document.body.offsetWidth,
+//     height = canvas.height = document.body.offsetHeight;
+    
+// }, 30);
 
 // listen keyboard
 document.onkeydown = (e) => {
